aaa722d32e2637b7c4a93f733585c48168d6cf42
phanlethanhvinh <88189280+phanlethanhvinh@users.noreply.github.com>
2021-07-30 17:21:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Em à!',
+    introDesc: `Code by ThanhVinh`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Yêu tuiiii ko 😙',
+    desc: 'TEST WEBITE ',
+    btnYes: 'Iuuuuuu nè <33',
+    btnNo: 'Có cái bìu :3',
+    question:'Vì sao bạn thích tao',
+    btnReply: 'Gửi cho Anh <3',
+    reply: 'Vinh đẹp trai quá điiiiiii <33333333',
+    mess: 'Anh biết mà 😘😘',
+    messDesc: 'Cho anh xin 50k đi, vô mess a gửi cái card viettel 50k nhé.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://facebook.com/thanhvinhofficial'
 }
