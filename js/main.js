41271d6edb3ba827c2cbdfea985719381f86b2da
Thành Vinh <88189280+taviny@users.noreply.github.com>
2022-06-26 15:48:36 +0700

    Update main.js
    vinh

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ function firstQuestion(){
     Swal.fire({
         title: CONFIG.introTitle,
         text: CONFIG.introDesc,
-        imageUrl: 'img/tavi.png',
+        imageUrl: 'img/tavi3.png',
         imageWidth: 300,
         imageHeight: 300,
         background: '#fff url("img/abcd.png")',
